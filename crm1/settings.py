9703c2554576fb6d16d9b33ca262465d0fb7abc3
sirdes <desmondtetteh1.dt@gmail.com>
2020-06-15 00:01:43 +0000

    second commit

--- crm1/settings.py
+++ crm1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +143,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'desmondtetteh1.dt@gmail.com'
 EMAIL_HOST_PASSWORD = '0277477720'
+
+# ACTIVATE DJANGO-HEROKU
+django_heroku.settings(locals())
